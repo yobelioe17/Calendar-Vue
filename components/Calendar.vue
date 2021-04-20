0f280cea593d7539d0bfa67936c91dbdaa374f44
Yobelio Ekaharja Putra <47659014+yobelioe17@users.noreply.github.com>
2021-04-20 22:54:20 +0700

    Initial commit

---
+++ components/Calendar.vue
@@ -0,0 +1,34 @@
+<template>
+	<full-calendar :config="config" :events="events"/>
+</template>
+
+<script>
+	import moment from 'moment'
+	export default {
+  name: 'hello',
+  data () {
+    return {
+			events: [
+				{
+					title: 'test',
+					allDay: true,
+					start: moment(),
+					end: moment().add(1, 'd'),
+				},
+				{
+					title: 'another test',
+					start: moment().add(2,'d'),
+					end: moment().add(2, 'd').add(2, 'h'),
+				},
+			],
+			config: {
+        defaultView: 'month',
+				eventRender: function(event, element) {
+        	console.log(event)
+    		}
+      },
+    }
+  }
+}
+
+</script>
